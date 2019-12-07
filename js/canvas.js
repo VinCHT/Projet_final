8c19ea9fd22188346df461042af41b74a22f764d
Vincent Champagnat <47330327+VinCHT@users.noreply.github.com>
2019-12-07 12:41:11 +0100

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -38,12 +38,14 @@ class Canvas {
         this.ctx.beginPath();
         this.ctx.moveTo(e.offsetX, e.offsetY);
         this.btnReservez.disabled = false;  
+        document.getElementById("reserver").style.display = "block"; 
         
     }
 
     effacer() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.btnReservez.disabled = true;  
+      
     }
  
     evenement() {
@@ -54,19 +56,19 @@ class Canvas {
         this.canvas.addEventListener('touchstart', this.onTouchStart.bind(this), false);
         this.canvas.addEventListener('touchend', this.onMouseUp.bind(this), false);
     }
+
     onTouchMove(event){
         if (this.mousePressed) {
             event.preventDefault();
             this.mouseX = (event.targetTouches[0].pageX) - this.canvas.offsetLeft;
             this.mouseY = (event.targetTouches[0].pageY) - this.canvas.offsetTop;
             this.ctx.lineWidth = this.penWidth.value;
-        this.ctx.strokeStyle = this.penColor.value;
+            this.ctx.strokeStyle = this.penColor.value;
             this.ctx.lineTo(this.mouseX, this.mouseY);
             this.ctx.stroke();
             this.btnReservez.disabled = false;  
         }
     }
-    
     
     onTouchStart(event){
         this.mousePressed = true;
@@ -80,7 +82,4 @@ class Canvas {
         this.mousePressed = false;
     }
   }
-  
-//   let monCanvas = new Canvas(document.getElementById('canvas'),this.canvas.getContext('2d'),document.querySelector('input[name="penColor"]'),document.querySelector('input[name="penWidth"]'),false,10,10,document.querySelector('#annuler'),document.getElementById('reserver'),false,0,0);
-//   monCanvas.evenement();
-  
+ 
